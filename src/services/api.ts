e84920bd43a350b243ccba6d8d119bc03ae3ed89
Paul Pessoa <paulmspessoa@gmail.com>
2024-12-02 15:20:26 -0300

    ajustado variavel de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,30 +1,13 @@
-// const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
-// export async function fetchImages() {
-//   try {
-//     const response = await fetch("");
-//     console.log(JSON.stringify(response));      
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error("Erro ao buscar dados:", error);
-//     return [];
-//   }
-// }
-
-
-
-import axios from 'axios';
-
-
+import axios from "axios";
 
 export async function fetchImages() {
   try {
-    const response = await axios.get("https://insta-bytes-back-204994694224.southamerica-east1.run.app/api/posts");
+    const response = await axios.get(`${API_URL}/posts`);
     return response.data;
   } catch (error) {
     console.error("Erro ao buscar dados:", error);
     return [];
   }
 }
-
